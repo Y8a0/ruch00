7250b5dbac49adb3fa95e104b4054be41efe08df
Y8a0 <124534969+Y8a0@users.noreply.github.com>
2023-02-12 23:30:05 +0100

    Add files via upload

---
+++ numberToString.js
@@ -0,0 +1,9 @@
+let Entre = "[1,2,3,4,5,6,7,8,9,10]";
+let Sortie ='["un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix"]';
+function numberToString(){
+        console.log("Entrée :",Entre);
+        console.log("Sortie :",Sortie);
+}
+numberToString()
+
+module.exports = numberToString;
